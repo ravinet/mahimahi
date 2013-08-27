d6f2684b5ba1c9c4adb3cc654a57352e02f56f2a
Ravi Netravali <ravinet@mit.edu>
2013-08-27 18:33:34 -0400

    Prints project name and version number

--- hello.cc
+++ hello.cc
@@ -4,11 +4,13 @@
 
 #include <iostream>
 #include <cstdlib>
+#include "config.h"
 
 using namespace std;
 
 int main( void )
 {
+    cout << "Welcome to " << PACKAGE_STRING << endl << endl; 
     cout << "Hello, world!" << endl;
     return EXIT_SUCCESS;
 }
