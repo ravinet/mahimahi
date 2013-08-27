a3e88b61f212d9d3642021d3fc721c368b23e2f6
Ravi Netravali <ravinet@mit.edu>
2013-08-27 17:58:32 -0400

    Successfully prints Hello, world! (Closes #1)

---
+++ hello.cc
@@ -0,0 +1,14 @@
+// Prints Hello, world!
+// By Ravi Netravali <ravinet@mit.edu>
+// Compile with: g++ -std=c++0x -g -O2 -Wall -Wextra -Weffc++ -Werror -pedantic -o hello hello.cc
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main( void )
+{
+    cout << "Hello, world!" << endl;
+    return EXIT_SUCCESS;
+}
