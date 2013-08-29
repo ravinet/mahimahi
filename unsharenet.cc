b64e4e34c563f5456326d0b5ef139cb05eb2ac07
Ravi Netravali <ravinet@mit.edu>
2013-08-29 09:59:15 -0400

    unshares network namespace and executes bash shell (must use sudo, lo still a NIC)

--- unsharenet.cc
+++ unsharenet.cc
@@ -5,11 +5,19 @@
 #include <sched.h>
 #include <iostream>
 #include <cstdlib>
+#include <unistd.h>
 
 using namespace std;
 
 int main ( void )
 {
+    // make array of character for exec system call { bash, 0}
+    char *argv[2];
+    string cmd = "bash";
+    argv[0] = const_cast<char*> (cmd.c_str());
+    argv[1] = 0;
+
+    // unshares network namespace (prints whether success or failure)
     if ( unshare( CLONE_NEWNET ) == -1 )
     {
         cout << "Error in unsharing" << endl;
@@ -17,6 +25,8 @@ int main ( void )
     else
     {
         cout << "Success in unsharing" << endl;
+        // executes bash shell
+        execv("/bin/bash", argv);
     }
     return EXIT_SUCCESS;
 }
