c2c573e0c53c997d46d5d1fc1de11efa28ea6fb2
Ravi Netravali <ravinet@mit.edu>
2013-09-09 11:34:57 -0400

    uses clock_gettime

--- packet.hh
+++ packet.hh
@@ -3,9 +3,9 @@ using namespace std;
 class Packet {
     public:
         // time packet should be sent
-        int timestamp;
+        uint64_t timestamp;
         string contents;
-        Packet(int i = 0, string j = ""):timestamp(i), contents(j) {};
-        int get_timestamp() const {return timestamp;}
+        Packet(uint64_t i = 0, string j = ""):timestamp(i), contents(j) {};
+        uint64_t get_timestamp() const {return timestamp;}
         string get_content() const {return contents;}
 };
