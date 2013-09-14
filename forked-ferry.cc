85619373cd72d2102f4e11680494429331fd103c
Keith Winstein <keithw@mit.edu>
2013-09-14 03:45:44 -0400

    Make forked-ferry use the new abstractions

--- forked-ferry.cc
+++ forked-ferry.cc
@@ -6,33 +6,91 @@
 #include <iostream>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <csignal>
 
 #include "tapdevice.hh"
 #include "exception.hh"
 #include "ferry_queue.hh"
+#include "child_process.hh"
+#include "signalfd.hh"
 
 using namespace std;
 
-void ferry( const FileDescriptor & tap, const FileDescriptor & sibling_fd, const uint64_t delay_ms )
+int handle_signal( const signalfd_siginfo & sig,
+                   ChildProcess & child_process )
 {
-    // set up poll for tap devices
-    struct pollfd pollfds[ 2 ];
+    switch ( sig.ssi_signo ) {
+    case SIGCONT:
+        /* resume child process too */
+        child_process.resume();
+        break;
+
+    case SIGCHLD:
+        /* make sure it's from the child process */
+        /* unfortunately sig.ssi_pid is a uint32_t instead of pid_t, so need to cast */
+        assert( sig.ssi_pid == static_cast<decltype(sig.ssi_pid)>( child_process.pid() ) );
+
+        /* figure out what happened to it */
+        child_process.wait();
+
+        if ( child_process.terminated() ) {
+            return child_process.exit_status();
+        } else if ( !child_process.running() ) {
+            /* suspend parent too */
+            if ( raise( SIGSTOP ) < 0 ) {
+                throw Exception( "raise" );
+            }
+        }
+        break;
+
+    case SIGHUP:
+    case SIGTERM:
+        child_process.signal( SIGHUP );
+
+        return EXIT_SUCCESS;
+        break;
+    default:
+        throw Exception( "unknown signal" );
+    }
+
+    return -1;
+}
+
+int ferry( const FileDescriptor & tap,
+           const FileDescriptor & sibling_fd,
+           ChildProcess & child_process,
+           const uint64_t delay_ms )
+{
+    /* set up signal file descriptor */
+    SignalMask signals_to_listen_for = { SIGCHLD, SIGCONT, SIGHUP, SIGTERM };
+    signals_to_listen_for.block(); /* don't let them interrupt us */
+
+    SignalFD signal_fd( signals_to_listen_for );
+
+    // set up poll
+    struct pollfd pollfds[ 3 ];
     pollfds[ 0 ].fd = tap.num();
     pollfds[ 0 ].events = POLLIN;
 
     pollfds[ 1 ].fd = sibling_fd.num();
     pollfds[ 1 ].events = POLLIN;
 
+    pollfds[ 2 ].fd = signal_fd.fd().num();
+    pollfds[ 2 ].events = POLLIN;
+
     FerryQueue delay_queue( delay_ms );
 
     while ( true ) {
         int wait_time = delay_queue.wait_time();
         
-        if ( poll( pollfds, 2, wait_time ) == -1 ) {
+        if ( poll( pollfds, 3, wait_time ) == -1 ) {
             throw Exception( "poll" );
         }
 
-        if ( (pollfds[ 0 ].revents | pollfds[ 1 ].revents) & POLLERR ) { /* check for error */
+        if ( (pollfds[ 0 ].revents
+              | pollfds[ 1 ].revents
+              | pollfds[ 2 ].revents )
+             & (POLLERR | POLLHUP | POLLNVAL) ) { /* check for error */
             throw Exception( "poll" );
         }
 
@@ -44,6 +102,16 @@ void ferry( const FileDescriptor & tap, const FileDescriptor & sibling_fd, const
         if ( pollfds[ 1 ].revents & POLLIN ) {
             /* packet FROM sibling goes to tap device */
             tap.write( sibling_fd.read() );
+        }
+
+        if ( pollfds[ 2 ].revents & POLLIN ) {
+            /* got a signal */
+            signalfd_siginfo sig = signal_fd.read_signal();
+
+            int return_value = handle_signal( sig, child_process );
+            if ( return_value >= 0 ) {
+                return return_value;
+            }
         }
 
         /* packets FROM tail of delay queue go to sibling */
@@ -63,35 +131,28 @@ int main( void )
             ingress_socket( pipes[ 1 ], "socketpair" );
 
         /* Fork */
-        pid_t child_pid = fork();
-        if ( child_pid < 0 ) {
-            throw Exception( "fork" );
-        }
-
-        if ( child_pid == 0 ) { /* child */
-            /* Unshare network namespace */
-            if ( unshare( CLONE_NEWNET ) == -1 ) {
-                throw Exception( "unshare" );
-            }
-
-            TapDevice ingress_tap( "ingress" );
-            /* Fork again */
-            pid_t grandchild_pid = fork();
-            if ( grandchild_pid < 0 ) {
-                throw Exception( "fork" );
-            }
-            if ( grandchild_pid == 0 ) { /* grandchild */
-                char* argv[ 2 ] = { const_cast<char *>( "bash" ), nullptr };
-                if ( execvp ( "bash", argv ) < 0 ) {
-                    throw Exception( "execvp" );
+        ChildProcess container_process( [&](){
+                /* Unshare network namespace */
+                if ( unshare( CLONE_NEWNET ) == -1 ) {
+                    throw Exception( "unshare" );
                 }
-            } else { /* child */
-                ferry( ingress_tap.fd(), egress_socket, 2500 );
-            }
-        } else { /* parent */
-            TapDevice egress_tap( "egress" );
-            ferry( egress_tap.fd(), ingress_socket, 2500 );
-        }
+
+                TapDevice ingress_tap( "ingress" );
+
+                /* Fork again */
+                ChildProcess bash_process( [](){
+                        char* argv[ 2 ] = { const_cast<char *>( "bash" ), nullptr };
+                        if ( execvp( "bash", argv ) < 0 ) {
+                            throw Exception( "execvp" );
+                        }
+                        return EXIT_FAILURE;
+                    } );
+
+                return ferry( ingress_tap.fd(), egress_socket, bash_process, 2500 );
+            } );
+
+        TapDevice egress_tap( "egress" );
+        return ferry( egress_tap.fd(), ingress_socket, container_process, 2500 );
     } catch ( const Exception & e ) {
         e.die();
     }
