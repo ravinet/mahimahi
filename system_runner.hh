4567c99f052dd62fb2729f0d5e3e8ea59cd9e5df
Keith Winstein <keithw@mit.edu>
2013-09-17 17:11:12 -0400

    Rewrite run() so it doesn't use system() -- we do it directly

--- system_runner.hh
+++ system_runner.hh
@@ -1,21 +1,30 @@
+/* -*-mode:c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
 #ifndef SYSTEM_RUNNER
 #define SYSTEM_RUNNER
 
 #include <string>
-#include <cstdlib>
+#include <unistd.h>
 
 #include "exception.hh"
 
 void run( const std::string & command )
 {
-  const int return_value = system( command.c_str() );
-  std::string error_message = std::string( "system: \"" ) + command.c_str() + '"';
-  if ( return_value < 0 ) {
-    throw Exception( error_message );
-  } else if ( return_value > 0 ) {
-    throw Exception( error_message, "command returned "
-		     + std::to_string( WEXITSTATUS( return_value ) ) );
-  }
+    ChildProcess command_process( [&] () -> int {
+            if ( execl( "/bin/sh", "/bin/sh", "-c", command.c_str(), static_cast<char *>( nullptr ) ) < 0 ) {
+                throw Exception( "execl" );
+            }
+            return EXIT_FAILURE;
+        } );
+
+    while ( !command_process.terminated() ) {
+        command_process.wait();
+    }
+
+    if ( command_process.exit_status() != 0 ) {
+        throw Exception( "`" + command + "'", "command returned "
+                         + std::to_string( command_process.exit_status() ) );
+    }
 }
 
 #endif /* SYSTEM_RUNNER */
