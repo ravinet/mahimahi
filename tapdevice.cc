6247919bd96f9dff9dc94f9e31276b01450af977
Keith Winstein <keithw@mit.edu>
2013-09-17 18:26:49 -0400

    Improve TapDevice with helper lambda

--- tapdevice.cc
+++ tapdevice.cc
@@ -6,6 +6,7 @@
 #include <sys/ioctl.h>
 #include <fcntl.h>
 #include <arpa/inet.h>
+#include <functional>
 
 #include "tapdevice.hh"
 #include "exception.hh"
@@ -18,80 +19,54 @@ TapDevice::TapDevice( const string & name,
                       const string & dstaddr )
     : fd_( open( "/dev/net/tun", O_RDWR ), "open /dev/net/tun" )
 {
+    auto ioctl_helper = [&]( FileDescriptor & ioctl_fd,
+                             const int ioctl_request,
+                             std::function<void( struct ifreq &ifr,
+                                                 struct sockaddr_in &sin )> ifr_adjustment )
     {
-        struct ifreq ifr;
-        memset( &ifr, 0, sizeof( ifr ) ); /* does not have default initializer */
-        strncpy( ifr.ifr_name, name.c_str(), IFNAMSIZ ); /* interface name */
-
-        ifr.ifr_flags = IFF_TUN; /* make tap device */
-
-        // create interface
-        if ( ioctl( fd().num(), TUNSETIFF, static_cast<void *>( &ifr ) ) < 0 ) {
-            throw Exception( "ioctl" );
-        }
-    }
-
-    {
-        struct ifreq ifr;
-        memset( &ifr, 0, sizeof( ifr ) ); /* does not have default initializer */
-        strncpy( ifr.ifr_name, name.c_str(), IFNAMSIZ ); /* interface name */
-
-        /* bring interface up */
-        ifr.ifr_flags += IFF_UP;
-
-        FileDescriptor sockfd( socket( AF_INET, SOCK_DGRAM, 0 ), "socket" );
-
-        if ( ioctl( sockfd.num(), SIOCSIFFLAGS, static_cast<void *>( &ifr ) ) < 0 ) {
-            throw Exception( "ioctl" );
-        }
-    }
-
-    {
-        /* assign given IP addresses to interface */
         struct ifreq ifr;
         memset( &ifr, 0, sizeof( ifr ) ); /* does not have default initializer */
         strncpy( ifr.ifr_name, name.c_str(), IFNAMSIZ ); /* interface name */
 
         struct sockaddr_in sin;
-        memset( &sin, 0, sizeof( sin ) ); /* does not have default initializer */
-
+        memset( &sin, 0, sizeof( sin ) ); /* does not have default initializer */    
         sin.sin_family = AF_INET;
 
-        if ( inet_aton( addr.c_str(), &sin.sin_addr ) == 0 ) {
-            throw Exception( "inet_aton" );
-        }
+        ifr_adjustment( ifr, sin );
 
-        memcpy( &ifr.ifr_addr, &sin, sizeof( struct sockaddr ) );
-
-        FileDescriptor sockfd( socket( AF_INET, SOCK_DGRAM, 0 ), "socket" );
-
-        if ( ioctl( sockfd.num(), SIOCSIFADDR, static_cast<void *>( &ifr ) ) < 0 ) {
+        if ( ioctl( ioctl_fd.num(), ioctl_request, static_cast<void *>( &ifr ) ) < 0 ) {
             throw Exception( "ioctl" );
         }
-    }
+    };
 
-    {
-        /* assign given destination IP addresses to interface */
-        struct ifreq ifr;
-        memset( &ifr, 0, sizeof( ifr ) ); /* does not have default initializer */
-        strncpy( ifr.ifr_name, name.c_str(), IFNAMSIZ ); /* interface name */
+    ioctl_helper( fd_, TUNSETIFF, [&] ( struct ifreq &ifr,
+                                        struct sockaddr_in & )
+                  { ifr.ifr_flags = IFF_TUN; } );
 
-        struct sockaddr_in sin;
-        memset( &sin, 0, sizeof( sin ) ); /* does not have default initializer */
+    FileDescriptor sockfd( socket( AF_INET, SOCK_DGRAM, 0 ), "socket" );
 
-        sin.sin_family = AF_INET;
+    /* bring interface up */
+    ioctl_helper( sockfd, SIOCSIFFLAGS, [&] ( struct ifreq &ifr,
+                                              struct sockaddr_in &)
+                  { ifr.ifr_flags = IFF_UP; } );
 
-        if ( inet_aton( dstaddr.c_str(), &sin.sin_addr ) == 0 ) {
-            throw Exception( "inet_aton" );
-        }
-
-        memcpy( &ifr.ifr_dstaddr, &sin, sizeof( struct sockaddr ) );
-
-        FileDescriptor sockfd( socket( AF_INET, SOCK_DGRAM, 0 ), "socket" );
-
-        if ( ioctl( sockfd.num(), SIOCSIFDSTADDR, static_cast<void *>( &ifr ) ) < 0 ) {
-            throw Exception( "ioctl" );
-        }
-    }
+    /* assign interface address */
+    ioctl_helper( sockfd, SIOCSIFADDR, [&] ( struct ifreq &ifr,
+                                             struct sockaddr_in &sin )
+                  {
+                      if ( inet_aton( addr.c_str(), &sin.sin_addr ) == 0 ) {
+                          throw Exception( "inet_aton" );
+                      }
+                      memcpy( &ifr.ifr_addr, &sin, sizeof( struct sockaddr ) );
+                  } );
+    /* assign destination addresses */
+    ioctl_helper( sockfd, SIOCSIFDSTADDR, [&] ( struct ifreq &ifr,
+                                                struct sockaddr_in &sin )
+                  {
+                      if ( inet_aton( dstaddr.c_str(), &sin.sin_addr ) == 0 ) {
+                          throw Exception( "inet_aton" );
+                      }
+                      memcpy( &ifr.ifr_dstaddr, &sin, sizeof( struct sockaddr ) );
+                  } );
 }
 
