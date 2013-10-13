9789b679c7cbcd1574ecdd404d24f173fd1bfc59
Ravi Netravali <ravinet@mit.edu>
2013-10-13 16:48:18 -0400

    prints original destination address of packet received

--- tcpnat.cc
+++ tcpnat.cc
@@ -6,102 +6,61 @@
 #include <string>
 #include <iostream>
 #include <utility>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <linux/netfilter_ipv4.h>
 
 #include "address.hh"
 #include "socket.hh"
 #include "timestamp.hh"
 #include "system_runner.hh"
+#include "config.h"
 
 using namespace std;
-/* set up DNAT which, for any tcp packet maps dest addr to ingress:80. Ingress then receives packet and sends the packet via egress and packet will come back to ingress which will know to send to source address */
-
-
-
-void service_request( const Socket & server_socket )
-{
-    try {
-        /* GET arrived at ingress...want socket to bind to ingress and connect to original destination address */
-        Socket outgoing_socket( SocketType::TCP );
-        outgoing_socket.connect( Address( "localhost", "domain", SocketType::TCP ) );
-
-        struct pollfd pollfds[ 2 ];
-        pollfds[ 0 ].fd = outgoing_socket.raw_fd();
-        pollfds[ 0 ].events = POLLIN;
-          
-        pollfds[ 1 ].fd = server_socket.raw_fd();
-        pollfds[ 1 ].events = POLLIN;
-
-        /* process requests until either socket is idle for 20 seconds or until EOF */
-        bool outgoing_eof = false;
-        bool server_eof   = false;
-        while( true ) {
-            if ( outgoing_eof || server_eof ) {
-                break;
-            }
-             if ( poll( pollfds, 2, 20000 ) < 0 ) {
-                throw Exception( "poll" );
-            }
-            if ( pollfds[ 1 ].revents & POLLIN ) {
-                /* read request, then send to local dns server */
-                string buffer = server_socket.read();
-                if ( buffer.empty() ) {
-                     server_eof = true;
-                }
-                outgoing_socket.write( buffer );
-            }
-            }
-           /* if response comes from local dns server, write back to source of request */
-           if ( pollfds[ 0 ].revents & POLLIN ) {
-                /* read response, then send back to client */
-                string buffer = outgoing_socket.read();
-                if ( buffer.empty() ) {
-                     outgoing_eof = true;
-                }
-                server_socket.write( buffer );
-            }
-        }
-    
-    } catch ( const Exception & e ) {
-        cerr.flush();
-        e.perror();
-        return;
-    }        
-
-    cerr.flush();
-    return;
-}
 
 int main( int argc, char *argv[] )
 {
-    if ( argc != 2 ) {
-        cerr << "Usage: " << argv[ 0 ] << " IP_ADDRESS_TO_BIND" << endl;
+    if ( argc != 3 ) {
+        cerr << "Usage: " << argv[ 0 ] << " IP_ADDRESS_TO_BIND" << "INTERFACE OF PROXY" << endl;
         return EXIT_FAILURE;
     }
 
     std::string ip_address_to_bind( argv[ 1 ] );
-    ip_address_to_bind = ip_address_to_bind + ":80";
+    std::string ip_port_to_bind(ip_address_to_bind + ":3333");
+
+    std::string interface_to_forward_to( argv[ 2 ] );
+    cout << "interface: " << interface_to_forward_to << endl;
 
     /* add entry to nat table using iptables to direct all TCP traffic to ingress */
-    run( { IPTABLES, "-t", "nat", "-A", "PREROUTING", "-p", "TCP", "-j", "DNAT", "--to-destination", ip_address_to_bind } );        
+    run( { IPTABLES, "-t", "nat", "-A", "PREROUTING", "-p", "TCP", "-i", interface_to_forward_to, "!", "--dport", "53", "-j", "DNAT", "--to-destination", ip_port_to_bind } );
 
-
-    /* make listener socket for dns requests */
     try {
         Socket listener_socket( SocketType::TCP );
-        /* bind to ingress ip and port 80 */
-        listener_socket.bind( Address( ip_address_to_bind, 80 ) );
-
-        cout << "hostname: " << listener_socket.local_addr().hostname() << endl;
-        cout << "port: " << listener_socket.local_addr().port() << endl;
+        /* bind to egress ip and port 3333 */
+        listener_socket.bind( Address( ip_address_to_bind, 3333 ) );
 
         /* listen on listener socket */
         listener_socket.listen();
+        while(1){
+            Socket proxy_socket( listener_socket.accept() );
+            struct sockaddr_in dstaddr;
+            socklen_t destlen = sizeof(dstaddr);
+            if (getsockopt( proxy_socket.raw_fd(), SOL_IP, SO_ORIGINAL_DST, &dstaddr, &destlen ) < 0) {
+                throw Exception( "getsockopt" );
+            }
+            struct pollfd pollfds[ 1 ];
+            pollfds[ 0 ].fd = proxy_socket.raw_fd();
+            pollfds[ 0 ].events = POLLIN;
 
-        while ( true ) {  /* service requests from this source  */
-            thread newthread( [] (const Socket & service_socket) -> void {
-                              service_request( service_socket ) ; },
-                              listener_socket.accept());
-            newthread.detach();
+            if ( poll( pollfds, 1, 50000 ) < 0 ) {
+                throw Exception( "poll" );
+            }
+
+            if ( pollfds[ 0 ].revents & POLLIN ) {
+                cout << "PACKET RECEIVED" << endl;
+                cout << "packet originally sent to: " << inet_ntoa( dstaddr.sin_addr ) << endl;
+            }
+
         }
     } catch ( const Exception & e ) {
         e.perror();
