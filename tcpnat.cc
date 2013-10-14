c97454e9a43a9f7e7c5d4d876c9113815db52249
Ravi Netravali <ravinet@mit.edu>
2013-10-14 18:51:31 -0400

    define function in thread, ferry packets between original source/dest

--- tcpnat.cc
+++ tcpnat.cc
@@ -18,84 +18,6 @@
 
 using namespace std;
 
-void service_request( const Socket & server_socket )
-{
-    try {
-        /* open socket to original destination
-        Socket outgoing_socket;
-        outgoing_socket.connect( Address( "localhost", "domain" ) ); */
-
-	struct sockaddr_in dstaddr;
-        socklen_t destlen = sizeof(dstaddr);
-        if (getsockopt( server_socket.raw_fd(), SOL_IP, SO_ORIGINAL_DST, &dstaddr, &destlen ) < 0) {
-            throw Exception( "getsockopt" );
-        }
-        struct pollfd pollfds[ 1 ];
-        pollfds[ 0 ].fd = server_socket.raw_fd();
-        pollfds[ 0 ].events = POLLIN;
-
-        if ( poll( pollfds, 1, 10000 ) < 0 ) {
-            throw Exception( "poll" );
-        }
-
-        if ( pollfds[ 0 ].revents & POLLIN ) {
-            cout << "packet originally sent to: " << inet_ntoa( dstaddr.sin_addr ) << endl;
-        }
-
-
-        /* poll on server_socket
-        struct pollfd pollfds[ 2 ];
-        pollfds[ 0 ].fd = outgoing_socket.raw_fd();
-        pollfds[ 0 ].events = POLLIN;
-
-        pollfds[ 1 ].fd = server_socket.raw_fd();
-        pollfds[ 1 ].events = POLLIN;
-
-        // process requests until either socket is idle for 20 seconds or until EOF
-        bool outgoing_eof = false;
-        bool server_eof = false;
-        while( true ) {
-            if ( outgoing_eof || server_eof ) {
-                break;
-            }
-            pollfds[ 0 ].events = outgoing_eof ? 0 : POLLIN;
-            pollfds[ 1 ].events = server_eof ? 0 : POLLIN;
-
-             if ( poll( pollfds, 2, 20000 ) < 0 ) {
-                throw Exception( "poll" );
-            }
-            if ( pollfds[ 1 ].revents & POLLIN ) {
-                // read request, then send to local dns server
-                string buffer = server_socket.read();
-                if ( buffer.empty() ) {
-                     server_eof = true;
-                }
-                outgoing_socket.write( buffer );
-            }
-            if ( poll( &pollfds[ 0 ], 1, 20000 ) < 0 ) {
-                throw Exception( "poll" );
-            }
-           // if response comes from local dns server, write back to source of request
-           if ( pollfds[ 0 ].revents & POLLIN ) {
-                // read response, then send back to client
-                string buffer = outgoing_socket.read();
-                if ( buffer.empty() ) {
-                     outgoing_eof = true;
-                }
-                server_socket.write( buffer );
-            }
-        }
-    */
-    } catch ( const Exception & e ) {
-        cerr.flush();
-        e.perror();
-        return;
-    }
-
-    cerr.flush();
-    return;
-}
-
 int main( int argc, char *argv[] )
 {
     if ( argc != 3 ) {
@@ -107,48 +29,67 @@ int main( int argc, char *argv[] )
     std::string ip_port_to_bind(ip_address_to_bind + ":3333");
 
     std::string interface_to_forward_to( argv[ 2 ] );
-    cout << "interface: " << interface_to_forward_to << endl;
 
     /* add entry to nat table using iptables to direct all TCP traffic to ingress */
     run( { IPTABLES, "-t", "nat", "-A", "PREROUTING", "-p", "TCP", "-i", interface_to_forward_to, "!", "--dport", "53", "-j", "DNAT", "--to-destination", ip_port_to_bind } );
+    Socket listener_socket( SocketType::TCP );
 
-    try {
-        Socket listener_socket( SocketType::TCP );
-        /* bind to egress ip and port 3333 */
-        listener_socket.bind( Address( ip_address_to_bind, 3333 ) );
+    /* bind to egress ip and port 3333 */
+    listener_socket.bind( Address( ip_address_to_bind, 3333 ) );
 
-        /* listen on listener socket */
-        listener_socket.listen();
-       /* while(1){
-            Socket proxy_socket( listener_socket.accept() );
-            struct sockaddr_in dstaddr;
-            socklen_t destlen = sizeof(dstaddr);
-            if (getsockopt( proxy_socket.raw_fd(), SOL_IP, SO_ORIGINAL_DST, &dstaddr, &destlen ) < 0) {
-                throw Exception( "getsockopt" );
+    /* listen on listener socket */
+    listener_socket.listen();
+
+    /* accept any connection request and then service all GETs */
+    while ( true ) {
+        thread newthread( [&] ( Socket server_socket ) {
+        try {
+            /* get original destination for connection request */
+            Address original_dst = server_socket.original_dest();
+	    cout << "connection intended for: " << original_dst.ip() << endl;
+
+	    /* create socket and connect to original destination and send original request */
+	    Socket original( SocketType::TCP );
+	    original.connect( original_dst );
+            original.write( server_socket.read() );
+
+            /* poll on original connect socket and new connection socket to ferry packets */
+            struct pollfd pollfds[ 2 ];
+	    /* socket connected to original destination */
+	    pollfds[ 0 ].fd = original.raw_fd();
+	    pollfds[ 0 ].events = POLLIN;
+	    /* socket connected to original source */
+	    pollfds[ 1 ].fd = server_socket.raw_fd();
+	    pollfds[ 1 ].events = POLLIN;
+
+            /* ferry packets between original source and original destination */
+            while(true) {
+	        if ( poll( pollfds, 1, 60000 ) < 0 ) {
+	            throw Exception( "poll" );
+                }
+
+                if ( pollfds[ 0 ].revents & POLLIN ) {
+                    string buffer = original.read();
+                    if ( buffer.empty() ) { break; } /* EOF */
+                    server_socket.write( buffer );
+                } else if ( pollfds[ 1 ].revents & POLLIN ) {
+                    string buffer = server_socket.read();
+                    if ( buffer.empty() ) { break; } /* EOF */
+                    original.write( buffer );
+                } else {
+                  break; /* timeout */
+                }
             }
-            struct pollfd pollfds[ 1 ];
-            pollfds[ 0 ].fd = proxy_socket.raw_fd();
-            pollfds[ 0 ].events = POLLIN;
-
-            if ( poll( pollfds, 1, 50000 ) < 0 ) {
-                throw Exception( "poll" );
-            }
-
-            if ( pollfds[ 0 ].revents & POLLIN ) {
-                cout << "PACKET RECEIVED" << endl;
-                cout << "packet originally sent to: " << inet_ntoa( dstaddr.sin_addr ) << endl;
-            }
-
-        }*/
-	/* accept any connection request and then service all GETs */
-        while ( true ) { /* service requests from this source */
-            thread newthread( [] (const Socket & service_socket) -> void {
-                              service_request( service_socket ) ; },
-                              listener_socket.accept());
-            newthread.detach();
+        } catch ( const Exception & e ) {
+            cerr.flush();
+            e.perror();
+            return;
         }
-    } catch ( const Exception & e ) {
-        e.perror();
-        return EXIT_FAILURE;
+        cerr.flush();
+        return;
+        }, listener_socket.accept() );
+
+        /* don't wait around for the reply */
+        newthread.detach();
     }
 }
