08746c99354dc3c8881ba54a8e2707af96ee2d63
Ravi Netravali <ravinet@mit.edu>
2013-10-15 16:31:39 -0400

    clean up unnecessary variables

--- dnat.hh
+++ dnat.hh
@@ -21,15 +21,12 @@ public:
         : listener( egress ),
           interface_to_forward_to( interface ) 
     {
-        std::string egress_ip_port = listener.ip() + ":" + std::to_string( listener.port() );
-        std::string egress_addr = egress.ip() + std::to_string( egress.port() );
-        run( { IPTABLES, "-t", "nat", "-A", "PREROUTING", "-p", "TCP", "-i", interface_to_forward_to, "!", "--dport", "53", "-j", "DNAT", "--to-destination", egress_ip_port } );
+        run( { IPTABLES, "-t", "nat", "-A", "PREROUTING", "-p", "TCP", "-i", interface_to_forward_to, "!", "--dport", "53", "-j", "DNAT", "--to-destination", listener.ip() + ":" + std::to_string( listener.port() ) } );
     }
 
     ~DNAT()
     {
-        std::string egress_ip_port = listener.ip() + ":" + std::to_string( listener.port() );
-        run( { IPTABLES, "-t", "nat", "-D", "PREROUTING", "-p", "TCP", "-i", interface_to_forward_to, "!", "--dport", "53", "-j", "DNAT", "--to-destination", egress_ip_port } );
+        run( { IPTABLES, "-t", "nat", "-D", "PREROUTING", "-p", "TCP", "-i", interface_to_forward_to, "!", "--dport", "53", "-j", "DNAT", "--to-destination", listener.ip() + ":" + std::to_string( listener.port() ) } );
     }
 };
 
