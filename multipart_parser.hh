5137dcaf313da600eb629161bcb40ac657af135b
Ravi Netravali <ravinet@mit.edu>
2013-11-25 16:59:51 -0500

    add comments, handle incomplete headers, parse range line if it does not have bytes

--- multipart_parser.hh
+++ multipart_parser.hh
@@ -16,19 +16,25 @@ private:
 
     bool part_pending_;
 
+    /* checks whether we have enough lines for complete part header (including boundary) */
     bool part_header_present( void );
 
+    /* pops boundary, content-type, content-range, and empty line */
     void pop_part_header( void );
 
+    /* returns size of part given Content-Range line */
     size_t get_part_size( const std::string & size_line );
 
+    /* incomplete header from previous buffer */
+    std::string previous_header_;
 
 public:
     MultipartBodyParser( void )
         : buffer_(),
           body_in_progress_(),
           part_size_(),
-          part_pending_( false )
+          part_pending_( false ),
+          previous_header_()
     {}
 
     size_t parse( const std::string & str, const std::string & boundary );
