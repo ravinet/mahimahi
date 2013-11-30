bb9cdc79514f90af7fc62bb28b69db9b7d96d740
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-11-30 00:23:22 -0500

    First cut of chunky parser

---
+++ chunked_parser.cc
@@ -0,0 +1,80 @@
+#include <assert.h>
+#include "ezio.hh"
+#include "chunked_parser.hh"
+
+using namespace std;
+
+/* Take a chunk header and parse it assuming no folding */
+uint32_t ChunkedBodyParser::get_chunk_size(const string & chunk_hdr) const
+{
+    /* Check that the chunk header ends with a CRLF */
+    assert(chunk_hdr.substr( chunk_hdr.length() - 2, 2 ) == "\r\n");
+
+    /* If there are chunk extensions, ';' terminates chunk size */
+    auto pos = chunk_hdr.find(";");
+
+    /* There are no ';'s, and hence no chunk externsions, CRLF terminates chunk size */
+    if (pos == string::npos) {
+        pos = chunk_hdr.find("\r\n");
+    }
+
+    /* Can't be npos even now */
+    assert( pos != string::npos );
+
+    /* Parse hex string */
+    return myatoi(chunk_hdr.substr(0, pos), 16);
+}
+
+/* Byte-at-a-time parser, might be slow, but conceptually simpler */
+bool ChunkedBodyParser::parse_byte( char byte )
+{
+    parser_buffer_.push_back( byte );
+    switch (state_) {
+    case CHUNK_HDR:
+        /* if you have CRLF, get chunk size & transition to CHUNK/TRAILER */
+        if (parser_buffer_.find("\r\n") != string::npos) {
+            /* get chunk size */
+            current_chunk_size_ = get_chunk_size( parser_buffer_ );
+
+            /* Transition appropriately */
+            state_ = (current_chunk_size_ == 0) ? TRAILER : CHUNK;
+
+            /* reset parser_buffer_ */
+            parser_buffer_ = {};
+        }
+        /* if you haven't seen a CRLF so far do nothing */
+        return false;
+
+    case CHUNK:
+        if (parser_buffer_.length() == current_chunk_size_ + 2) {
+          /* accumulated enough bytes, check CR/LF */
+          assert(parser_buffer_.substr( parser_buffer_.length() - 2, 2 ) == "\r\n");
+
+          /* Transition to next state */
+          state_ = CHUNK_HDR;
+
+          /* reset parser_buffer_ */
+          parser_buffer_ = {};
+        }
+        return false;
+
+    case TRAILER:
+        /* If we find two consectuve CRLFs, we are done */
+        return (trailers_enabled_) ? (parser_buffer_.find("\r\n\r\n") != string::npos)
+                                   : (parser_buffer_.find("\r\n") != string::npos);
+
+    default:
+        assert( false );
+        return false;
+    }
+}
+
+string::size_type ChunkedBodyParser::read( const std::string & input_buffer )
+{
+  for (uint32_t i = 0; i < input_buffer.length(); i++) {
+      if (parse_byte( input_buffer.at( i ) )) {
+          return i + 1; /* TODO: i or i + 1 ? */
+      }
+  }
+  return string::npos;
+}
--- chunked_parser.hh
+++ chunked_parser.hh
@@ -4,20 +4,27 @@
 #define CHUNKED_BODY_PARSER_HH
 
 #include "body_parser.hh"
+#include "exception.hh"
 
 class ChunkedBodyParser : public BodyParser
 {
+private:
+    uint32_t get_chunk_size(const std::string & chunk_hdr) const;
+    bool parse_byte(char byte);
+    std::string parser_buffer_ {""};
+    uint32_t current_chunk_size_ {0};
+    enum {CHUNK_HDR, CHUNK, TRAILER} state_ {CHUNK_HDR};
+    bool trailers_enabled_ {false};
+
 public:
-    std::string::size_type read( const std::string & ) override
-    {
-        throw Exception( "ChunkedBodyParser", "chunked body not yet supported" );
-    }
+    std::string::size_type read( const std::string & ) override;
 
     bool eof( void ) override
     {
         throw Exception( "ChunkedBodyParser", "invalid EOF in the middle of body" );
         return false;
     };
+    ChunkedBodyParser(bool t_trailers_enabled) : trailers_enabled_( t_trailers_enabled ) {};
 };
 
 #endif /* CHUNKED_BODY_PARSER_HH */
