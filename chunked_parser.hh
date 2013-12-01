2d97dc645982935c9b7f3a4287b7e33b99cb9c5b
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-12-01 00:26:32 -0500

    New version of ChunkedBodyParser that processes entire strings, and not byte-at-a-time.

--- chunked_parser.hh
+++ chunked_parser.hh
@@ -9,12 +9,16 @@
 class ChunkedBodyParser : public BodyParser
 {
 private:
+    std::string::size_type compute_ack_size(const std::string & haystack,
+                                            const std::string & needle,
+                                            std::string::size_type input_size);
     uint32_t get_chunk_size(const std::string & chunk_hdr) const;
-    bool parse_byte(char byte);
     std::string parser_buffer_ {""};
     uint32_t current_chunk_size_ {0};
+    std::string::size_type acked_so_far_ {0};
+    std::string::size_type parsed_so_far_ {0};
     enum {CHUNK_HDR, CHUNK, TRAILER} state_ {CHUNK_HDR};
-    bool trailers_enabled_ {false};
+    const bool trailers_enabled_ {false};
 
 public:
     std::string::size_type read( const std::string & ) override;
@@ -24,6 +28,7 @@ public:
         throw Exception( "ChunkedBodyParser", "invalid EOF in the middle of body" );
         return false;
     };
+
     ChunkedBodyParser(bool t_trailers_enabled) : trailers_enabled_( t_trailers_enabled ) {};
 };
 
