3c30e48a663f6736162d2a8d2fc112e3d71be43b
Keith Winstein <keithw@mit.edu>
2014-01-08 17:06:27 -0500

    ReadWriteInterface needs virtual destructor to shut down sockets correctly

--- read_write_interface.hh
+++ read_write_interface.hh
@@ -1,5 +1,7 @@
-#ifndef READ_WRITE_INTERFACE_H
-#define READ_WRITE_INTERFACE_H
+/* -*-mode:c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+#ifndef READ_WRITE_INTERFACE_HH
+#define READ_WRITE_INTERFACE_HH
 
 #include <string>
 #include "file_descriptor.hh"
@@ -14,6 +16,8 @@ public:
     virtual std::string read( void ) = 0;
     virtual void write( const std::string & str ) = 0;
     virtual FileDescriptor & fd( void ) = 0;
+
+    virtual ~ReadWriteInterface() {}
 };
 
 #endif
