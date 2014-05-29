7fc0d279337aed630cdee635980fd0b8985b69a6
Ravi Netravali <ravinet@mit.edu>
2014-05-29 15:04:18 -0400

    add timing to python script

--- regression_tests/persistent_requests.py
+++ regression_tests/persistent_requests.py
@@ -1,10 +1,16 @@
 #must run "sudo pip install requests"
 #if that doesnt work, first do "sudo pip install --upgrade setuptools" 
 import requests
+from time import time
+
+start = time()
 s = requests.Session()
 r = s.get('http://nms.csail.mit.edu/')
 #r = s.get('http://www.mit.edu/')
 print r.status_code
 r = s.get('http://nms.csail.mit.edu/nms_logo.jpg')
+end = time()
 #r = s.get('http://www.mit.edu/favicon.ico')
 print r.status_code
+total = end - start
+print total
