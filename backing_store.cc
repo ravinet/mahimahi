697d0739d192e367118a8bd646e0242ed5643944
Keith Winstein <keithw@mit.edu>
2014-07-14 16:57:34 -0400

    Refactor backing stores into their own class

---
+++ backing_store.cc
@@ -0,0 +1,33 @@
+/* -*-mode:c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+#include "backing_store.hh"
+#include "http_record.pb.h"
+#include "temp_file.hh"
+
+using namespace std;
+
+HTTPDiskStore::HTTPDiskStore( const string & record_folder )
+    : record_folder_( record_folder )
+{}
+
+void HTTPDiskStore::save( const HTTPResponse & response, const Address & server_address )
+{
+    /* output file to write current request/response pair protobuf (user has all permissions) */
+    UniqueFile file( record_folder_ + "save" );
+
+    /* construct protocol buffer */
+    MahimahiProtobufs::RequestResponse output;
+
+    output.set_ip( server_address.ip() );
+    output.set_port( server_address.port() );
+    output.set_scheme( server_address.port() == 443
+                       ? MahimahiProtobufs::RequestResponse_Scheme_HTTPS
+                       : MahimahiProtobufs::RequestResponse_Scheme_HTTP );
+    output.mutable_request()->CopyFrom( response.request().toprotobuf() );
+    output.mutable_response()->CopyFrom( response.toprotobuf() );
+
+    if ( not output.SerializeToFileDescriptor( file.fd().num() ) ) {
+        throw Exception( "save_to_disk", "failure to serialize HTTP request/response pair" );
+    }
+
+}
---
+++ backing_store.hh
@@ -0,0 +1,29 @@
+/* -*-mode:c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+#ifndef BACKING_STORE_HH
+#define BACKING_STORE_HH
+
+#include <string>
+
+#include "http_request.hh"
+#include "http_response.hh"
+#include "address.hh"
+
+/* abstract base class to store an HTTP request/response from a particular server address */
+class HTTPBackingStore
+{
+public:
+    virtual void save( const HTTPResponse & response, const Address & server_address ) = 0;
+};
+
+class HTTPDiskStore : public HTTPBackingStore
+{
+private:
+    std::string record_folder_;
+
+public:
+    HTTPDiskStore( const std::string & record_folder );
+    void save( const HTTPResponse & response, const Address & server_address ) override;
+};
+
+#endif /* BACKING_STORE_HH */
