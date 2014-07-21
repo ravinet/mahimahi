beb06ab796b99da1bf37c4321a0cd0d89d4e3c9c
Ravi Netravali <ravinet@mit.edu>
2014-07-21 09:19:56 -0400

    Rename HTTPBulkResponse to HTTPConsoleStore

--- http_console_store.cc
+++ http_console_store.cc
@@ -1,16 +1,16 @@
 /* -*-mode:c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
-#include "http_bulk_response.hh"
+#include "http_console_store.hh"
 #include "http_record.pb.h"
 #include "temp_file.hh"
 
 using namespace std;
 
-HTTPBulkResponse::HTTPBulkResponse()
+HTTPConsoleStore::HTTPConsoleStore()
     : mutex_()
 {}
 
-void HTTPBulkResponse::save( const HTTPResponse & response, const Address & server_address )
+void HTTPConsoleStore::save( const HTTPResponse & response, const Address & server_address )
 {
     unique_lock<mutex> ul( mutex_ );
     cout << "Request to " << server_address.ip() << ": " << response.request().first_line() << ", Response: " << response.first_line() << endl;
--- http_console_store.hh
+++ http_console_store.hh
@@ -1,7 +1,7 @@
 /* -*-mode:c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
-#ifndef HTTP_BULK_RESPONSE_HH
-#define HTTP_BULK_RESPONSE_HH
+#ifndef HTTP_CONSOLE_STORE_HH
+#define HTTP_CONSOLE_STORE_HH
 
 #include <string>
 #include <mutex>
@@ -13,14 +13,14 @@
 
 /* class to print an HTTP request/response to screen */
 
-class HTTPBulkResponse : public HTTPBackingStore
+class HTTPConsoleStore : public HTTPBackingStore
 {
 private:
     std::mutex mutex_;
 
 public:
-    HTTPBulkResponse();
+    HTTPConsoleStore();
     void save( const HTTPResponse & response, const Address & server_address ) override;
 };
 
-#endif /* HTTP_BULK_RESPONSE_HH */
+#endif /* HTTP_CONSOLE_STORE_HH */
