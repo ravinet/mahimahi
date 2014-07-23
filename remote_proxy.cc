8e0130196d5bd60898193c504ea9f30d0b343888
Ravi Netravali <ravinet@mit.edu>
2014-07-23 13:45:54 -0400

    server/client poll in both directions...counter names veth pair (not pid)

--- remote_proxy.cc
+++ remote_proxy.cc
@@ -2,6 +2,7 @@
 
 #include <sys/socket.h>
 #include <unistd.h>
+#include <string>
 
 #include "util.hh"
 #include "url_loader.hh"
@@ -15,6 +16,49 @@
 using namespace std;
 using namespace PollerShortNames;
 
+void handle_client( Socket & client, const int & veth_counter )
+{
+    HTTPRequestParser request_parser;
+
+    Poller poller;
+
+    URLLoader load_page;
+
+    bool done_loading = false;
+
+    poller.add_action( Poller::Action( client, Direction::In,
+                                       [&] () {
+                                           string buffer = client.read();
+                                           request_parser.parse( buffer );
+                                           return ResultType::Continue;
+                                       },
+                                       [&] () { return true; } ) );
+
+    poller.add_action( Poller::Action( client, Direction::Out,
+                                       [&] () {
+                                           client.write( "DONE" );
+                                           done_loading = false;
+                                           return Result::Type::Exit;
+                                       },
+                                       [&] () { return done_loading; } ) );
+
+    while ( true ) {
+        if ( not request_parser.empty() )  { /* we have a complete request */
+            string url;
+            HTTPRequest incoming_request = request_parser.front();
+            request_parser.pop();
+            if ( incoming_request.has_header( "Host" ) ) {
+                url = incoming_request.get_header_value( "Host" );
+            }
+            load_page.get_all_resources( url, veth_counter );
+            done_loading = true;
+        }
+        if ( poller.poll( -1 ).result == Poller::Result::Type::Exit ) {
+            return;
+        }
+    }
+}
+
 int main( int argc, char *argv[] )
 {
     try {
@@ -27,43 +71,32 @@ int main( int argc, char *argv[] )
             throw Exception( "Usage", string( argv[ 0 ] ) );
         }
 
+        /* Use this as part of veth interface name because pid is same for different clients */
+        int veth_counter = 0;
+
         Socket listener_socket( TCP );
         listener_socket.bind( Address( "0.0.0.0", 5555 ) );
         listener_socket.listen();
 
-        Socket client = listener_socket.accept();
-
-        Poller poller;
-
-        HTTPRequestParser request_parser;
-
-        /* completed requests from client are serialized and sent to server */
-        poller.add_action( Poller::Action( client, Direction::In,
-                                           [&] () {
-                                               string buffer = client.read();
-                                               request_parser.parse( buffer );
-                                               return ResultType::Continue;
-                                           },
-                                           [&] () { return true; } ) );
+        Poller poll_for_clients;
 
         EventLoop pageload_loop;
 
+        /* listen for new clients */
+        poll_for_clients.add_action( Poller::Action( listener_socket, Direction::In,
+                                                     [&] () {
+                                                         Socket client = listener_socket.accept();
+                                                         pageload_loop.add_child_process( "new_client", [&] () {
+                                                             handle_client( client, veth_counter );
+                                                             return EXIT_SUCCESS;
+                                                         } );
+                                                         veth_counter++;
+                                                         return ResultType::Continue;
+                                                     },
+                                                     [&] () { return true; } ) );
+
         while ( true ) {
-            if ( not request_parser.empty() )  { /* we have a complete request */
-                string url;
-                HTTPRequest incoming_request = request_parser.front();
-                cout << incoming_request.str();
-                request_parser.pop();
-                if ( incoming_request.has_header( "Host" ) ) {
-                    url = incoming_request.get_header_value( "Host" );
-                }
-                pageload_loop.add_child_process( "url", [&] () {
-                    URLLoader load_page;
-                    load_page.get_all_resources( url );
-                    return EXIT_SUCCESS;
-                } );
-            }
-            if ( poller.poll( -1 ).result == Poller::Result::Type::Exit ) {
+            if ( poll_for_clients.poll( -1 ).result == Poller::Result::Type::Exit ) {
                 return EXIT_SUCCESS;
             }
         }
