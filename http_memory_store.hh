4c6c7f11dc6a30ba1b743c17a8e0ba76dfb4a6dd
Ravi Netravali <ravinet@mit.edu>
2014-07-24 16:55:20 -0400

    First cut of HTTPMemoryStore

---
+++ http_memory_store.hh
@@ -0,0 +1,30 @@
+/* -*-mode:c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+#ifndef HTTP_MEMORY_STORE_HH
+#define HTTP_MEMORY_STORE_HH
+
+#include <string>
+#include <mutex>
+#include <utility>
+
+#include "http_request.hh"
+#include "http_response.hh"
+#include "address.hh"
+#include "backing_store.hh"
+
+/* class to print an HTTP request/response to screen */
+
+class HTTPMemoryStore : public HTTPBackingStore
+{
+private:
+    std::mutex mutex_;
+    MahimahiProtobufs::BulkMessage requests;
+    MahimahiProtobufs::BulkMessage responses;
+
+public:
+    HTTPMemoryStore();
+    void save( const HTTPResponse & response, const Address & server_address );
+    std::pair<std::string, std::string> serialize( void );
+};
+
+#endif /* HTTP_MEMORY_STORE_HH */
