61b80ff10ac0ffe41b9d537d88754ab83f052bca
Ravi Netravali <ravinet@mit.edu>
2014-08-01 13:56:48 -0400

    Make local proxy a server which listens for HTTP requests

--- local_proxy.cc
+++ local_proxy.cc
@@ -9,8 +9,11 @@
 #include "http_header.hh"
 #include "int32.hh"
 #include "http_request.hh"
+#include "http_request_parser.hh"
+#include "poller.hh"
 
 using namespace std;
+using namespace PollerShortNames;
 
 int match_length( const string & first, const string & second )
 {
@@ -61,37 +64,12 @@ MahimahiProtobufs::HTTPMessage find_response( MahimahiProtobufs::BulkMessage & r
     throw Exception( "test_client", "Could not find matching request for: " + new_request.first_line() );
 }
 
-int main( int argc, char *argv[] )
+void handle_client( Socket && client, const Address & remote_proxy, const string & scheme )
 {
-    try {
-        if ( argc != 6 ) {
-            throw Exception( "Usage", string( argv[ 0 ] ) + " host port scheme hostname path" );
-        }
-
         Socket server( TCP );
-        server.connect( Address( argv[ 1 ], argv[ 2 ] ) );
+        server.connect( remote_proxy );
 
-        /* parse url into scheme and HTTPMessage request */
-
-        if ( string( argv[ 5 ] ).at( 0 ) != '/' ) {
-            throw Exception( string( argv[ 0 ] ), "path must begin with /" );
-        }
-
-        /* Construct bulk request */
-        MahimahiProtobufs::BulkRequest bulk_request;
-        bulk_request.set_scheme( string( argv[ 3 ] ) == "https" ? MahimahiProtobufs::BulkRequest_Scheme_HTTPS : MahimahiProtobufs::BulkRequest_Scheme_HTTP );
-        MahimahiProtobufs::HTTPMessage request_message;
-        request_message.set_first_line( "GET " + string( argv[ 5 ] ) + " HTTP/1.1" );
-        HTTPHeader request_header( "Host: " + string( argv[ 4 ] ) );
-        request_message.add_header()->CopyFrom( request_header.toprotobuf() );
-        bulk_request.mutable_request()->CopyFrom( request_message );
-        string request_proto;
-        bulk_request.SerializeToString( &request_proto );
-
-        /* Make request string to send (prepended with size) */
-        string request = static_cast<string>( Integer32( request_proto.size() ) ) + request_proto;
-
-        server.write( request );
+        HTTPRequestParser request_parser;
 
         LengthValueParser bulk_parser;
 
@@ -100,19 +78,88 @@ int main( int argc, char *argv[] )
         MahimahiProtobufs::BulkMessage requests;
         MahimahiProtobufs::BulkMessage responses;
 
-        while ( not server.eof() ) {
-            auto res = bulk_parser.parse( server.read() );
-            if ( res.first ) { /* We read a complete bulk protobuf */
-                if ( not parsed_requests ) {
-                    requests.ParseFromString( res.second );
-                    parsed_requests = true;
-                } else {
-                    responses.ParseFromString( res.second );
-                }
+        MahimahiProtobufs::BulkRequest bulk_request;
+
+        Poller poller;
+
+        poller.add_action( Poller::Action( client, Direction::In,
+                                           [&] () {
+                                               string buffer = client.read();
+                                               request_parser.parse( buffer );
+                                               return ResultType::Continue;
+                                           },
+                                           [&] () { return true; } ) );
+
+        poller.add_action( Poller::Action( server, Direction::Out,
+                                           [&] () {
+                                               bulk_request.set_scheme( scheme == "https" ? MahimahiProtobufs::BulkRequest_Scheme_HTTPS :
+                                                                                            MahimahiProtobufs::BulkRequest_Scheme_HTTP );
+                                               bulk_request.mutable_request()->CopyFrom( request_parser.front().toprotobuf() );
+                                               request_parser.pop();
+                                               string request_proto;
+                                               bulk_request.SerializeToString( &request_proto );
+
+                                               /* Make request string to send (prepended with size) */
+                                               string request = static_cast<string>( Integer32( request_proto.size() ) ) + request_proto;
+
+                                               server.write( request );
+
+                                               return ResultType::Continue;
+                                           },
+                                           [&] () { return not request_parser.empty(); } ) );
+
+        poller.add_action( Poller::Action( server, Direction::In,
+                                           [&] () {
+                                               string buffer = server.read();
+                                               auto res = bulk_parser.parse( buffer );
+                                               if ( res.first ) { /* we have read a complete bulk protobuf */
+                                                   if ( not parsed_requests ) { /* it is the requests */
+                                                       requests.ParseFromString( res.second );
+                                                       parsed_requests = true;
+                                                   } else { /* it is the responses */
+                                                       responses.ParseFromString( res.second );
+                                                       MahimahiProtobufs::HTTPMessage matched_response = find_response( requests, responses, bulk_request );
+                                                       client.write( matched_response.first_line() );
+                                                   }
+                                               }
+                                               return ResultType::Continue;
+                                           },
+                                           [&] () { return not server.eof(); } ) );
+
+        while ( true ) {
+            if ( poller.poll( -1 ).result == Poller::Result::Type::Exit ) {
+                return;
             }
         }
-        MahimahiProtobufs::HTTPMessage matched_response = find_response( requests, responses, bulk_request );
-        cout << matched_response.first_line() << endl;
+}
+
+int main( int argc, char *argv[] )
+{
+    try {
+        if ( argc != 5 ) {
+            throw Exception( "Usage", string( argv[ 0 ] ) + " local_proxy_port remote_proxy_ip remote_proxy_port scheme" );
+        }
+
+        Address remote_proxy( string( argv[ 2 ] ), argv[ 3 ] );
+
+        Socket listener_socket( TCP );
+        listener_socket.bind( Address( "0", argv[ 1 ] ) );
+        listener_socket.listen();
+
+        EventLoop event_loop;
+
+        /* listen for new clients */
+        event_loop.add_simple_input_handler( listener_socket,
+                                             [&] () {
+                                                 Socket client = listener_socket.accept();
+                                                 event_loop.add_child_process( ChildProcess( "new_client", [&] () {
+                                                         handle_client( move( client ), remote_proxy, string( argv[ 4 ] ) );
+                                                         return EXIT_SUCCESS;
+                                                     } ), false );
+                                                 return ResultType::Continue;
+                                             } );
+
+        return event_loop.loop();
     } catch ( const Exception & e ) {
         e.perror();
         return EXIT_FAILURE;
