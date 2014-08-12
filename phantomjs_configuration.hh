cd58fae9049013f31f3f980cabe9dd7407beb0ba
Ravi Netravali <ravinet@mit.edu>
2014-08-12 10:42:29 -0400

    Phantomjs exits with return 0 when page load status is 'Fail'

--- phantomjs_configuration.hh
+++ phantomjs_configuration.hh
@@ -5,6 +5,6 @@
 #ifndef PHANTOMJS_CONFIGURATION_HH
 #define PHANTOMJS_CONFIGURATION_HH
 
-std::string phantomjs_config = "\"\nvar system = require('system')\nvar page = require('webpage').create();\npage.settings.userAgent = 'Chrome/31.0.1650.63 Safari/537.36';\npage.customHeaders = {\"accept\": \"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\"};\npage.open(url, function (status) {\nif (status != 'success') {\nconsole.log('Unable to load address')\n} else {\nwindow.setTimeout(function () {\nphantom.exit(0);\n}, 200);\n}\n});";
+std::string phantomjs_config = "\";\nvar system = require('system');\nvar page = require('webpage').create();\npage.settings.userAgent = 'Chrome/31.0.1650.63 Safari/537.36';\npage.customHeaders = {\"accept\": \"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\"};\npage.open(url, function (status) {\nif (status != 'success') {\nconsole.log('Unable to load address: ' + url);\nphantom.exit(0);\n} else {\nwindow.setTimeout(function () {\nphantom.exit(0);\n}, 200);\n}\n});";
 
 #endif /* PHANTOMJS_CONFIGURATION_HH */
