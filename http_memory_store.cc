7e4874d22ae260292b8e47ac64149afb4e5cd65f
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-09-09 14:25:16 -0400

    Rewrite code in serialize_to_socket to make it clearer

--- http_memory_store.cc
+++ http_memory_store.cc
@@ -31,33 +31,35 @@ void HTTPMemoryStore::save( const HTTPResponse & response, const Address & serve
 
 void HTTPMemoryStore::serialize_to_socket( Socket && client )
 {
-    /* bulk response format: # of pairs, request protobuf size, request protobuf, # of pairs, response protobuf size, response protobuf */
-
+    /* bulk response format:
+       length-value pair for first response,
+       length-value pair for requests protobuf,
+       for each response:
+         length-value pair for each response
+     */
     unique_lock<mutex> ul( mutex_ );
 
     /* make sure there is a 1 to 1 matching of requests and responses */
     assert( requests.msg_size() == responses.msg_size() );
 
-    /* Serialize both request and response to String for transmission */
-    string all_requests;
-    requests.SerializeToString( &all_requests );
-
-    /* Put sizes and messages into bulk response formats (one for request, one for response) */
-    string requests_ret = static_cast<string>( Integer32( all_requests.size() ) ) + all_requests;
-
-    string responses_ret;
-
-    for ( int i = 0; i < responses.msg_size(); i++ ) { /* add response string size and response string */
-        string current_response;
-        responses.msg( i ).SerializeToString( &current_response );
-        responses_ret = responses_ret + static_cast<string>( Integer32( current_response.size() ) ) + current_response;
-    }
-
-    /* prepend the first response size and the response itself to the requests of bulk response */
+    /* First, write out the first response as a length-value pair */
     string first_response;
     responses.msg( 0 ).SerializeToString( &first_response );
-    string requests_and_response = static_cast<string>( Integer32( first_response.size() ) ) + first_response + requests_ret;
+    first_response = static_cast<string>( Integer32( first_response.size() ) ) + first_response;
 
-    client.write( requests_and_response );
-    client.write( responses_ret );
+    /* Serialize all requests alone to a length-value pair for transmission */
+    string all_requests;
+    requests.SerializeToString( &all_requests );
+    all_requests = static_cast<string>( Integer32( all_requests.size() ) ) + all_requests;
+
+    /* Now make a set of length-value pairs, one for each response */
+    string all_responses;
+    for ( int i = 0; i < responses.msg_size(); i++ ) {
+        /* add response string size and response string */
+        string current_response;
+        responses.msg( i ).SerializeToString( &current_response );
+        all_responses = all_responses + static_cast<string>( Integer32( current_response.size() ) ) + current_response;
+    }
+
+    client.write( first_response + all_requests + all_responses );
 }
