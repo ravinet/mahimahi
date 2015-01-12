e7124c46e00f12a1d9e556c142791d9f1eb1c51d
Ravi Netravali <ravinet@mit.edu>
2015-01-12 17:08:16 -0500

    Completely scripted to rewrite stored site (w/o rewrite logic)

--- modify_site.py
+++ modify_site.py
@@ -2,24 +2,61 @@ import os
 import sys
 import subprocess
 
+# recorded folder to be copied and rewritten
 recorded_folder = sys.argv[1]
 
-# list files in specified directory
-files = os.listdir(recorded_folder);
+# folder to store rewritten protobufs
+os.system( "cp -r " + recorded_folder + " rewritten" )
+
+files = os.listdir("rewritten")
 
 for filename in files:
     print filename
-    command = "protototext " + recorded_folder + "/" + filename + " " + recorded_folder + "/tempfile"
+
+    # convert response in protobuf to text (ungzip if necessary)
+    command = "protototext rewritten/" + filename + " rewritten/tempfile"
     proc = subprocess.Popen([command], stdout=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
     return_code = proc.returncode
     out = out.strip("\n")
     print out
     # need to still handle if response is chunked and gzipped (we can't just run gzip on it)!
-    if ( out == "gzipped" ):
-        os.system("gzip -d -c " + recorded_folder + "/tempfile > " + recorded_folder + "/plaintext")
-        # file "plaintext" in recorded_folder has plain text version of recorded response. for now we delete
-        # it but we want to run our rewriter on that and then make it a new file in a new 'recorded folder' (using texttoproto)
-        # and then change the content length accordingly
-        os.system("rm " + recorded_folder + "/plaintext")
-    os.system("rm " + recorded_folder + "/tempfile")
+    if ( ("html" in out) or ("javascript" in out) ): # html or javascript file, so rewrite
+        if ( "chunked" in out ): # response chunked so we must unchunk
+            os.system( "python unchunk.py rewritten/tempfile rewritten/tempfile1" )
+            os.system( "mv rewritten/tempfile1 rewritten/tempfile" )
+            # remove transfer-encoding chunked header from original file since we are unchunking
+            os.system( "removeheader rewritten/" + filename + " Transfer-Encoding" )
+        if ( "not" in out ): # html or javascript but not gzipped
+            # REWRITE TEMPFILE HERE
+
+            # get new length of response
+            size = os.path.getsize('rewritten/tempfile')
+
+            # convert modified file back to protobuf
+            os.system( "texttoproto rewritten/tempfile rewritten/" + filename )
+
+            # add new content length header
+            os.system( "changeheader rewritten/" + filename + " Content-Length " + str(size) )
+        else: # gzipped
+            os.system("gzip -d -c rewritten/tempfile > rewritten/plaintext")
+
+            # REWRITE PLAINTEXT HERE
+
+            # after modifying plaintext, gzip it again (gzipped file is 'finalfile')
+            os.system( "gzip -c rewritten/plaintext > rewritten/finalfile" )
+
+            # get new length of response
+            size = os.path.getsize('rewritten/finalfile')
+
+            # convert modified file back to protobuf
+            os.system( "texttoproto rewritten/finalfile rewritten/" + filename )
+
+            # add new content length header to the newly modified protobuf (name is filename)
+            os.system( "changeheader rewritten/" + filename + " Content-Length " + str(size) )
+
+            # delete temp files
+            os.system("rm rewritten/plaintext")
+            os.system("rm rewritten/finalfile")
+    # delete original tempfile
+    os.system("rm rewritten/tempfile")
