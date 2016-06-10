e75d0283d71cf68b79598b820c86463d70fc59a9
Ravi Netravali <ravinet@mit.edu>
2016-06-10 16:27:40 -0400

    allow delayshell to take in a file that maps ip addrs to delays for syn packets

--- delay_queue.cc
+++ delay_queue.cc
@@ -4,11 +4,33 @@
 
 #include "delay_queue.hh"
 #include "timestamp.hh"
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
 
 using namespace std;
 
 void DelayQueue::read_packet( const string & contents )
 {
+    string no_tun_contents = contents.substr(4);
+
+    struct iphdr *iph = (struct iphdr *)no_tun_contents.c_str();
+    struct sockaddr_in dest;
+    memset(&dest, 0, sizeof(dest));
+    dest.sin_addr.s_addr = iph->daddr;
+    unsigned short iphdrlen;
+    iphdrlen = iph->ihl*4;
+    struct tcphdr *tcph=(struct tcphdr*)(no_tun_contents.c_str() + iphdrlen);
+    if ( tcph->syn == 1 ) {
+        std::map<string, float>::iterator it;
+        it = delay_map.find(inet_ntoa(dest.sin_addr));
+        if ( it != delay_map.end() ) {
+            //cout << "delaying by: " << it->second << "for SYN: " << tcph->syn << ", DEST: " << inet_ntoa(dest.sin_addr) << ", PROT: " << iph->protocol << "PORT: " << ntohs(tcph->dest) << endl;
+            usleep((it->second)*1000);
+        }
+    }
+    //cout << "SYN: " << tcph->syn << ", DEST: " << inet_ntoa(dest.sin_addr) << ", PROT: " << (unsigned int) iph->protocol << "PORT: " << ntohs(tcph->dest) << endl;
+
     packet_queue_.emplace( timestamp() + delay_ms_, contents );
 }
 
--- delay_queue.hh
+++ delay_queue.hh
@@ -6,7 +6,10 @@
 #include <queue>
 #include <cstdint>
 #include <string>
-
+#include <fstream>
+#include <map>
+#include <fcntl.h>
+#include <unistd.h>
 #include "file_descriptor.hh"
 
 class DelayQueue
@@ -15,9 +18,25 @@ private:
     uint64_t delay_ms_;
     std::queue< std::pair<uint64_t, std::string> > packet_queue_;
     /* release timestamp, contents */
+    std::map<std::string, float>delay_map = {};
 
 public:
-    DelayQueue( const uint64_t & s_delay_ms ) : delay_ms_( s_delay_ms ), packet_queue_() {}
+    DelayQueue( const uint64_t & s_delay_ms ) : delay_ms_( s_delay_ms ), packet_queue_(), delay_map() {
+        std::string comp_file = "delay_ip_mapping";
+        std::ifstream dfile(comp_file);
+        if ( dfile ) {
+            std::ifstream ifile;
+            ifile.open( comp_file );
+            std::string curr_line;
+            while ( getline( ifile, curr_line ) ) {
+                std::string ip = curr_line.substr(0, curr_line.find(" "));
+                float time = stof(curr_line.substr(curr_line.find(" ")));
+                delay_map[ip] = time;
+                //std::cout << "ADDED ip: " << ip << " with time: " << time << std::endl;
+            }
+            ifile.close();
+        }
+    }
 
     void read_packet( const std::string & contents );
 
