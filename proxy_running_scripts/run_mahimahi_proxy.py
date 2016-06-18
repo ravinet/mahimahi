4fa567587114347fb799af5c9359c17279db92a9
Vaspol Ruamviboonsuk <vaspol@umich.edu>
2016-06-18 21:41:56 +0000

    modified proxy running scripts

--- proxy_running_scripts/run_mahimahi_proxy.py
+++ proxy_running_scripts/run_mahimahi_proxy.py
@@ -4,22 +4,30 @@ from argparse import ArgumentParser
 
 import subprocess
 import os
+import time
 
 CONFIG = 'proxy_running_config'
 
 PROXY_REPLAY_PATH = 'mm-proxyreplay'
+PHONE_RECORD_PATH = 'mm-phone-webrecord'
 NGHTTPX_PATH = 'nghttpx'
 NGHTTPX_PORT = 'nghttpx_port'
 NGHTTPX_KEY = 'nghttpx_key'
 NGHTTPX_CERT = 'nghttpx_cert'
 BASE_RECORD_DIR = 'base_record_dir'
+BASE_RESULT_DIR = 'base_result_dir'
 
 MM_PROXYREPLAY = 'mm-proxyreplay'
+MM_PHONE_WEBRECORD = 'mm-phone-webrecord'
 NGHTTPX = 'nghttpx'
 APACHE = 'apache'
 PAGE = 'page'
+SQUID_PORT = 'squid_port'
+SQUID = 'squid'
 
-CONFIG_FIELDS = [ PROXY_REPLAY_PATH, NGHTTPX_PATH, NGHTTPX_PORT, NGHTTPX_KEY, NGHTTPX_CERT, BASE_RECORD_DIR ]
+TIME = 'time'
+
+CONFIG_FIELDS = [ PROXY_REPLAY_PATH, NGHTTPX_PATH, NGHTTPX_PORT, NGHTTPX_KEY, NGHTTPX_CERT, BASE_RECORD_DIR, PHONE_RECORD_PATH, SQUID_PORT, BASE_RESULT_DIR ]
 
 app = Flask(__name__)
 
@@ -27,7 +35,8 @@ app = Flask(__name__)
 def start_proxy():
     print proxy_config
     page = request.args[PAGE]
-    path_to_recorded_page = os.path.join(proxy_config[BASE_RECORD_DIR], escape_page(page))
+    request_time = request.args[TIME]
+    path_to_recorded_page = os.path.join(proxy_config[BASE_RECORD_DIR], request_time, escape_page(page))
     # cmd: ./mm-proxyreplay ../../page_loads/apple.com/ ./nghttpx 10000 ../certs/nghttpx_key.pem ../certs/nghttpx_cert.pem
     command = '{0} {1} {2} {3} {4} {5}'.format(
                                            proxy_config[PROXY_REPLAY_PATH], \
@@ -45,8 +54,42 @@ def stop_proxy():
     processes = [ MM_PROXYREPLAY, NGHTTPX, APACHE ]
     for process in processes:
         command = 'pkill {0}'.format(process)
-        subprocess.call(command, shell=True)
+        subprocess.Popen(command, shell=True)
     return 'Proxy Stopped'
+
+@app.route("/start_recording")
+def start_recording():
+    print 'start recording'
+    page = request.args[PAGE]
+    request_time = request.args[TIME]
+    mkdir_cmd = 'mkdir -p {0}'.format(os.path.join(proxy_config[BASE_RECORD_DIR], request_time))
+    subprocess.call(mkdir_cmd, shell=True)
+    record_path = os.path.join(proxy_config[BASE_RECORD_DIR], request_time, escape_page(page))
+    if os.path.exists(record_path):
+        rm_cmd = 'rm -r {0}'.format()
+        subprocess.call(rm_cmd, shell=True)
+    command = '{0} {1} {2}'.format(
+                            proxy_config[PHONE_RECORD_PATH], \
+                            os.path.join(proxy_config[BASE_RECORD_DIR], request_time, escape_page(page)), \
+                            proxy_config[SQUID_PORT])
+    process = subprocess.Popen(command, shell=True)
+    return 'Proxy Started'
+
+@app.route("/stop_recording")
+def stop_recording():
+    processes = [ MM_PHONE_WEBRECORD, SQUID ]
+    for process in processes:
+        command = 'pkill {0}'.format(process)
+        print command
+        subprocess.Popen(command, shell=True)
+    return 'Proxy Stopped'
+
+@app.route("/is_recording")
+def is_recording():
+    command = 'pgrep "mm-phone-webrecord"'
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    return stdout
 
 @app.route("/is_proxy_running")
 def is_proxy_running():
@@ -54,6 +97,13 @@ def is_proxy_running():
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     stdout, stderr = process.communicate()
     return stdout
+
+@app.route("/done")
+def done():
+    command = 'mv {0}/* {1}'.format(proxy_config[BASE_RECORD_DIR], proxy_config[BASE_RESULT_DIR])
+    process = subprocess.Popen(command, shell=True)
+    print 'DONE'
+    return 'OK', 200
 
 def get_proxy_config(config_filename):
     config = dict()
