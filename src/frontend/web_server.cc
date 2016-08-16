348ab247a423ebed6beb512f52b2f4e2670d2f9f
Vaspol Ruamviboonsuk <vaspol@umich.edu>
2016-08-16 15:35:17 -0400

    Modified webserver files to support adding Link headers

--- src/frontend/web_server.cc
+++ src/frontend/web_server.cc
@@ -4,6 +4,11 @@
 #include <iostream>
 #include <unistd.h>
 
+#include <fstream>
+#include <map>
+#include <vector>
+#include <sstream>
+
 #include "web_server.hh"
 #include "apache_configuration.hh"
 #include "system_runner.hh"
@@ -13,83 +18,140 @@
 
 using namespace std;
 
+void write_config_file(TempFile & config_file, const Address & addr, 
+                       const string & working_directory, const string & record_path) {
+    cout << "Apache Config File: " << config_file.name() << " listening on: " << addr.str() <<  endl;
+    config_file.write( apache_main_config );
+
+    string path_prefix = PATH_PREFIX;
+    config_file.write("LoadModule unixd_module " + path_prefix + "/modules/mod_unixd.so\n");
+    config_file.write("LoadModule log_config_module " + path_prefix + "/modules/mod_log_config.so\n");
+    config_file.write("LoadModule mpm_prefork_module " + path_prefix + "/modules/mod_mpm_prefork.so\n");
+
+    config_file.write( "WorkingDir " + working_directory + "\n" );
+    config_file.write( "RecordingDir " + record_path + "\n" );
+
+    /* add pid file, log files, user/group name, and listen line to config file and run apache */
+    config_file.write( "PidFile /tmp/replayshell_apache_pid." + to_string( getpid() ) + "." + to_string( random() ) + "\n" );
+    /* Apache will check if this file exists before clobbering it,
+       so we think it's ok for Apache to write here as root */
+
+    config_file.write( "ServerName mahimahi.\n" );
+
+    config_file.write( "ErrorLog " + path_prefix + "/logs/apache_errors.log\n" );
+
+    config_file.write( "CustomLog " + path_prefix + "/logs/custom.log common\n" );
+
+    config_file.write( "User #" + to_string( getuid() ) + "\n" );
+
+    config_file.write( "Group #" + to_string( getgid() ) + "\n" );
+
+    config_file.write( "Listen " + addr.str() );
+}
+
 WebServer::WebServer( const Address & addr, const string & working_directory, const string & record_path )
     : config_file_( "/tmp/replayshell_apache_config" ),
       moved_away_( false )
 {
-    cout << "Apache Config File: " << config_file_.name() << " listening on: " << addr.str() <<  endl;
-    config_file_.write( apache_main_config );
+    // cout << "Apache Config File: " << config_file_.name() << " listening on: " << addr.str() <<  endl;
+    // config_file_.write( apache_main_config );
 
-    string path_prefix = PATH_PREFIX;
-    config_file_.write("LoadModule unixd_module " + path_prefix + "/modules/mod_unixd.so\n");
-    config_file_.write("LoadModule log_config_module " + path_prefix + "/modules/mod_log_config.so\n");
-    config_file_.write("LoadModule mpm_prefork_module " + path_prefix + "/modules/mod_mpm_prefork.so\n");
+    // string path_prefix = PATH_PREFIX;
+    // config_file_.write("LoadModule unixd_module " + path_prefix + "/modules/mod_unixd.so\n");
+    // config_file_.write("LoadModule log_config_module " + path_prefix + "/modules/mod_log_config.so\n");
+    // config_file_.write("LoadModule mpm_prefork_module " + path_prefix + "/modules/mod_mpm_prefork.so\n");
 
-    config_file_.write( "WorkingDir " + working_directory + "\n" );
-    config_file_.write( "RecordingDir " + record_path + "\n" );
+    // config_file_.write( "WorkingDir " + working_directory + "\n" );
+    // config_file_.write( "RecordingDir " + record_path + "\n" );
+
+    // /* add pid file, log files, user/group name, and listen line to config file and run apache */
+    // config_file_.write( "PidFile /tmp/replayshell_apache_pid." + to_string( getpid() ) + "." + to_string( random() ) + "\n" );
+    // /* Apache will check if this file exists before clobbering it,
+    //    so we think it's ok for Apache to write here as root */
+
+    // config_file_.write( "ServerName mahimahi.\n" );
+
+    // config_file_.write( "ErrorLog " + path_prefix + "/logs/apache_errors.log\n" );
+
+    // config_file_.write( "CustomLog " + path_prefix + "/logs/custom.log common\n" );
+
+    // config_file_.write( "User #" + to_string( getuid() ) + "\n" );
+
+    // config_file_.write( "Group #" + to_string( getgid() ) + "\n" );
+
+    // config_file_.write( "Listen " + addr.str() );
+    write_config_file(config_file_, addr, working_directory, record_path);
 
     /* if port 443, add ssl components */
     if ( addr.port() == 443 ) { /* ssl */
         config_file_.write( apache_ssl_config );
     }
 
-    /* add pid file, log files, user/group name, and listen line to config file and run apache */
-    config_file_.write( "PidFile /tmp/replayshell_apache_pid." + to_string( getpid() ) + "." + to_string( random() ) + "\n" );
-    /* Apache will check if this file exists before clobbering it,
-       so we think it's ok for Apache to write here as root */
-
-    config_file_.write( "ServerName mahimahi.\n" );
-
-    config_file_.write( "ErrorLog " + path_prefix + "/logs/apache_errors.log\n" );
-
-    config_file_.write( "CustomLog " + path_prefix + "/logs/custom.log common\n" );
-
-    config_file_.write( "User #" + to_string( getuid() ) + "\n" );
-
-    config_file_.write( "Group #" + to_string( getgid() ) + "\n" );
-
-    config_file_.write( "Listen " + addr.str() );
-
     run( { APACHE2, "-f", config_file_.name(), "-k", "start" } );
 }
 
-WebServer::WebServer( const Address & addr, const string & working_directory, const string & record_path, const string & ssl_key, const string & ssl_cert )
+void split(const string &s, char delim, vector<string> &elems) {
+  stringstream ss(s);
+  string item;
+  while (getline(ss, item, delim)) {
+    elems.push_back(item);
+  }
+}
+
+vector<string> split(const string &s, char delim) {
+  vector<string> elems;
+  split(s, delim, elems);
+  return elems;
+}
+
+void populate_push_configurations( TempFile & config_file, const string & dependency_file ) {
+  map< string, vector< string >> dependencies_map;
+  ifstream infile(dependency_file);
+  string line;
+  if (infile.is_open()) {
+    while (getline(infile, line)) {
+      vector< string > splitted_line = split(line, ' ');
+      if (dependencies_map.find(splitted_line[0]) == dependencies_map.end()) {
+        dependencies_map[splitted_line[0]] = { };
+      }
+      dependencies_map[splitted_line[0]].push_back(splitted_line[2]);
+    }
+    infile.close();
+  }
+
+  if ( !dependencies_map.empty() ) {
+    // Write the dependencies to the configuration file.
+    config_file.write("<IfModule mod_headers.c>\n");
+    for (auto it = dependencies_map.begin(); it != dependencies_map.end(); ++it) {
+      auto key = it->first;
+      auto values = it->second;
+      config_file.write("<Location \"" + key  + "\">\n"); // Set location condition.
+      for (auto list_it = values.begin(); list_it != values.end(); ++list_it) {
+        // Push all dependencies for the location.
+        string link_string = "Link: <" + *list_it + ">; rel=preload";
+        config_file.write("Header add " + link_string + "\n");
+      }
+      config_file.write("</Location>\n"); 
+    }
+    config_file.write("</IfModule>\n");
+  }
+}
+
+WebServer::WebServer( const Address & addr, const string & working_directory,
+           const string & record_path, const string & dependency_file )
     : config_file_( "/tmp/replayshell_apache_config" ),
       moved_away_( false )
 {
-    cout << "Apache Config File: " << config_file_.name() << " listening on: " << addr.str() <<  endl;
-    config_file_.write( apache_main_config );
+    write_config_file(config_file_, addr, working_directory, record_path);
 
-    string path_prefix = PATH_PREFIX;
-    config_file_.write("LoadModule unixd_module " + path_prefix + "/modules/mod_unixd.so\n");
-    config_file_.write("LoadModule log_config_module " + path_prefix + "/modules/mod_log_config.so\n");
-    config_file_.write("LoadModule mpm_prefork_module " + path_prefix + "/modules/mod_mpm_prefork.so\n");
-
-    config_file_.write( "WorkingDir " + working_directory + "\n" );
-    config_file_.write( "RecordingDir " + record_path + "\n" );
+    config_file_.write( "LoadModule headers_module modules/mod_headers.so" );
+    
+    populate_push_configurations(config_file_, dependency_file);
 
     /* if port 443, add ssl components */
     if ( addr.port() == 443 ) { /* ssl */
-        string apache_ssl_custom_config = "LoadModule ssl_module " + std::string( MOD_SSL ) + "\nSSLEngine on\nSSLCertificateFile      " + ssl_cert + "\nSSLCertificateKeyFile " + ssl_key +"\n";
-        config_file_.write( apache_ssl_custom_config );
+        config_file_.write( apache_ssl_config );
     }
-
-    /* add pid file, log files, user/group name, and listen line to config file and run apache */
-    config_file_.write( "PidFile /tmp/replayshell_apache_pid." + to_string( getpid() ) + "." + to_string( random() ) + "\n" );
-    /* Apache will check if this file exists before clobbering it,
-       so we think it's ok for Apache to write here as root */
-
-    config_file_.write( "ServerName mahimahi.\n" );
-
-    config_file_.write( "ErrorLog /dev/null\n" );
-
-    config_file_.write( "CustomLog /dev/null common\n" );
-
-    config_file_.write( "User #" + to_string( getuid() ) + "\n" );
-
-    config_file_.write( "Group #" + to_string( getgid() ) + "\n" );
-
-    config_file_.write( "Listen " + addr.str() );
 
     run( { APACHE2, "-f", config_file_.name(), "-k", "start" } );
 }
--- src/frontend/web_server.hh
+++ src/frontend/web_server.hh
@@ -18,8 +18,8 @@ private:
 
 public:
     WebServer( const Address & addr, const std::string & working_directory, const std::string & record_path );
-
     WebServer( const Address & addr, const std::string & working_directory, const std::string & record_path, const std::string & ssl_key, const std::string & ssl_cert );
+    WebServer( const Address & addr, const std::string & working_directory, const std::string & record_path, const std::string & dependency_file );
 
     ~WebServer();
 
